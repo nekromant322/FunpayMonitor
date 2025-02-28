c7cf034d7dfea347f7baa2e2d8413df92fc41b54
Боткин_Кирилл <worldloflwarcraft@mail.ru>
2025-02-28 17:23:53 +0400

    настроил кеширование запроса к binance и освобождение кеша со временем

--- src/main/java/com/overridetech/funpay_monitor/client/api/BinanceClient.java
+++ src/main/java/com/overridetech/funpay_monitor/client/api/BinanceClient.java
@@ -2,9 +2,13 @@ package com.overridetech.funpay_monitor.client.api;
 
 import com.overridetech.funpay_monitor.dto.CurrencyRateDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
@@ -13,16 +17,19 @@ public class BinanceClient {
     private static final String BINANCE_SYMBOL_PRICE_BASE_URL = "https://api.binance.com";
     private final RestClient restClient = RestClient.builder().baseUrl(BINANCE_SYMBOL_PRICE_BASE_URL).build();
 
+
+    @Cacheable(cacheNames = "currency-rate", key = "#symbol")
     public CurrencyRateDto getCurrencyRates(String symbol) {
-        try {
-            return restClient.get()
-                    .uri("/api/v3/ticker/price?symbol=" + symbol)
-                    .header("Accept", "application/json")
-                    .retrieve()
-                    .body(CurrencyRateDto.class);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Failed to fetch currency rate for symbol:" + symbol, e);
-        }
+        return restClient.get()
+                .uri("/api/v3/ticker/price?symbol=" + symbol)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError,
+                        (request, response) -> {
+                            String errorBody = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
+                            throw new IllegalArgumentException("Failed to fetch currency rate for symbol:" + symbol + "\n" + errorBody);
+                        })
+                .body(CurrencyRateDto.class);
     }
 
 }
--- src/main/java/com/overridetech/funpay_monitor/config/AppConfig.java
+++ src/main/java/com/overridetech/funpay_monitor/config/AppConfig.java
@@ -1,8 +1,30 @@
 package com.overridetech.funpay_monitor.config;
 
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
 @Configuration
 public class AppConfig {
+
+
+
+    @Bean
+    public Caffeine caffeineConfig(){
+        return Caffeine.newBuilder().expireAfterWrite(20, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine){
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
 }
