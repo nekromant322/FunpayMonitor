29d140af541231548ea28a55d6faecf5177d5b55
Боткин_Кирилл <worldloflwarcraft@mail.ru>
2025-03-02 21:01:35 +0400

    реализовал парсинг расценок на rub/usd  с помощью селениум

--- src/main/java/com/overridetech/funpay_monitor/service/CurrencyService.java
+++ src/main/java/com/overridetech/funpay_monitor/service/CurrencyService.java
@@ -2,6 +2,7 @@ package com.overridetech.funpay_monitor.service;
 
 import com.overridetech.funpay_monitor.client.api.BinanceClient;
 import com.overridetech.funpay_monitor.dto.CurrencyRateDto;
+import com.overridetech.funpay_monitor.selenium.FunpayParser;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CurrencyService {
 
+    private final FunpayParser funpayParser;
     private final BinanceClient binanceClient;
+
+    public CurrencyRateDto getCurrencyRatesWithSelenium() {
+        return funpayParser.getExcangeRate();
+    }
 
     @SneakyThrows
     public CurrencyRateDto getCurrencyRatesFor() {
