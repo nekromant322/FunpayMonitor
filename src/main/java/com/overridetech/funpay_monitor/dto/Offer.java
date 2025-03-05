51e6b996c9d12e753a967e00e087a4d9dbd8fc03
Боткин_Кирилл <worldloflwarcraft@mail.ru>
2025-03-05 21:50:30 +0400

    сделал фильтр универсальным для всех оферов, встроел его в добавление спаршенных данных в бд

---
+++ src/main/java/com/overridetech/funpay_monitor/dto/Offer.java
@@ -0,0 +1,8 @@
+package com.overridetech.funpay_monitor.dto;
+
+public interface Offer {
+
+    public String getPrice();
+    public String getStock();
+
+}
--- src/main/java/com/overridetech/funpay_monitor/service/FunPayService.java
+++ src/main/java/com/overridetech/funpay_monitor/service/FunPayService.java
@@ -1,14 +1,17 @@
 package com.overridetech.funpay_monitor.service;
 
 import com.overridetech.funpay_monitor.client.FunPayClient;
+import com.overridetech.funpay_monitor.dto.FunPayPoe2Offer;
 import com.overridetech.funpay_monitor.mapper.Poe2DtoToEntityMapper;
 import com.overridetech.funpay_monitor.parser.FunPayPoe2Parser;
 import com.overridetech.funpay_monitor.repository.Poe2DivineOfferRepository;
+import com.overridetech.funpay_monitor.util.filter.OutliersFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -18,18 +21,21 @@ public class FunPayService {
 
     private final FunPayClient client;
     private final Poe2DivineOfferRepository repository;
+    private final OutliersFilter<FunPayPoe2Offer> outliersFilter;
 
     public void scrapDivineOffers() {
         try {
             LocalDateTime time = LocalDateTime.now();
             var htmls = client.getHtmls();
 
-            var dtos = htmls.stream()
+            List<FunPayPoe2Offer> dtos = htmls.stream()
                     .map(FunPayPoe2Parser::parseHtmlToFunPayPoe2Offer)
                     .filter(Objects::nonNull)
                     .filter(dto -> dto.getItem().toLowerCase().contains("божеств"))
                     .peek(dto -> dto.setTime(time))
                     .toList();
+
+            dtos = outliersFilter.trimDataSet(dtos, 0.1, 0.5, 20d);
 
             var offers = dtos.stream()
                     .filter(Objects::nonNull)
